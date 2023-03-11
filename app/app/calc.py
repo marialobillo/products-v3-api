3953163374349119c457831f2a677b6e6f7b63f7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-03-11 22:28:29 +0100

    feat: substract two numbers

--- app/app/calc.py
+++ app/app/calc.py
@@ -4,3 +4,8 @@
 def add(a, b):
     """ Add two numbers """
     return a + b
+
+
+def substract(a, b):
+    """ Substract two numbers """
+    return a - b
--- app/app/tests.py
+++ app/app/tests.py
@@ -7,3 +7,8 @@ class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
         """ Test that two numbers are added together """
         self.assertEqual(calc.add(5, 6), 11)
+
+    def test_substract_numbers(self):
+        """ Test substracting two numbers"""
+        res = calc.substract(16, 10)
+        self.assertEqual(res, 6)
